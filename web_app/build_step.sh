80151d89a096398b83c692429f7c478cbcc10bca
auman66 <alex.goldman@picknik.ai>
2021-12-27 15:20:46 -0700

    add build step file

--- web_app/build_step.sh
+++ web_app/build_step.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+# echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+# Always build if it's a web dev or staging branch
+if [[ "$VERCEL_GIT_COMMIT_REF" == "web-dev" || "$VERCEL_GIT_COMMIT_REF" == "web-staging" ]] ; then
   # Proceed with the build
-    echo "✅ - Build can proceed"
+    echo "✅ - Build can proceed - Web Branch"
   exit 1;
 
+# Build when there is a change to the web folder, unless the branch is main
+elif [[ `git diff HEAD^ HEAD .`  && "$VERCEL_GIT_COMMIT_REF" != "main" ]]
+    # Proceed with the build
+    echo "✅ - Build can proceed - Change to web folder"
+  exit 1;
+
+elif [[ ]]
 else
   # Don't build
   echo "🛑 - Build cancelled"
+  exit 0;
+fi
+
+if [[ `git diff HEAD^ HEAD .` ]] ; then
+    echo "✅ - Build can proceed 2"
+    exit 1;
+
+else
+  # Don't build
+  echo "🛑 - Build cancelled 2"
   exit 0;
 fi
